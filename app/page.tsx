77f9ed3fb77d5de7a77246173e04808a2988e1fa
Hoon <dt@live.it>
2023-06-29 15:23:30 +0800

    feat: join a room when user click join button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -7,8 +10,14 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import { nanoid } from 'nanoid';
 
 export default function Home() {
+  const [roomId, setRoomId] = useState<string>('');
+  const router = useRouter();
+
+  const joinRoom = () => router.push(`/room/${roomId || nanoid(10)}`);
+
   return (
     <main className='flex items-center justify-center h-screen w-screen'>
       <Card className='w-[350px]'>
@@ -16,10 +25,15 @@ export default function Home() {
           <CardTitle>Join a room</CardTitle>
         </CardHeader>
         <CardContent>
-          <Input id='roomId' placeholder='Enter Room ID...' />
+          <Input
+            id='roomId'
+            placeholder='Enter Room ID...'
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          />
         </CardContent>
         <CardFooter>
-          <Button>Join</Button>
+          <Button onClick={joinRoom}>Join</Button>
         </CardFooter>
       </Card>
     </main>
